c9c24950a0c70cce6d06fb3dccffe07df58990a9
Sriram R <sriram15690@mail.com>
2015-09-17 20:40:27 +0530

    first spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,4 @@
 FactoryGirl.define do
-  factory :user do
-   
   factory :valid_user, :class => :user do
     email {"user_#{rand(1000).to_s}@example.com"}
     password "12345678"
@@ -15,11 +13,11 @@ FactoryGirl.define do
   end
 
   
-  factory :valid_booking , :class =>:booking do |f|
+  factory :booking1 , :class =>:booking do |f|
     f.table_id 1
     f.user_id 1
     f.start_time "02/09/2017 20:00"
-    f.duration 15
+    f.end_time "02/09/2017 20:30"
   end
   
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Booking, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
     it  " should not create booking without 60 minutes gap" do
-      booking = FActoryGr
+      curr_booking = FactoryGirl.build(:booking1)
+      expect(curr_booking).to be_valid
     end
 end
