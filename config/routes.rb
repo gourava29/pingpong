6da3d4250e6a9e743849b912cec8606a4ca1fad5
Gourav Agarwal <gourava29@yahoo.co.in>
2015-09-17 14:55:15 +0000

    Added timestamp support

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'bookings#index'
   resources :bookings
   resources :tables
+  get "getAllbooking" => "bookings#getAll"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
