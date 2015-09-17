ed5b19e6e59c2384a98aa3fe9fff56ce4adda82b
Sriram R <sriram15690@mail.com>
2015-09-17 20:51:33 +0530

    first spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -20,4 +20,39 @@ FactoryGirl.define do
     f.end_time "02/09/2017 20:30"
   end
   
+  factory :booking2 , :class =>:booking do |f|
+    f.table_id 1
+    f.user_id 1
+    f.start_time "02/09/2017 20:50"
+    f.end_time "02/09/2017 21:20"
+  end
+  
+   factory :booking3 , :class =>:booking do |f|
+    f.table_id 1
+    f.user_id 1
+    f.start_time "02/09/2017 20:50"
+    f.end_time "02/09/2017 23:20"
+  end
+  
+   factory :booking4 , :class =>:booking do |f|
+    f.table_id 1
+    f.user_id 1
+    f.start_time "02/09/2017 08:00"
+    f.end_time "02/09/2017 08:10"
+  end
+  
+  factory :booking5 , :class =>:booking do |f|
+    f.table_id 1
+    f.user_id 1
+    f.start_time "01/09/2017 08:00"
+    f.end_time "01/09/2017 08:20"
+  end
+  
+   factory :booking6 , :class =>:booking do |f|
+    f.table_id 1
+    f.user_id 1
+   f.start_time "01/09/2017 08:10"
+    f.end_time "01/09/2017 08:30"
+  end
+  
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,9 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
+  
+  before :each do 
+    Booking.delete_all
+  end
   pending "add some examples to (or delete) #{__FILE__}"
-    it  " should not create booking without 60 minutes gap" do
+   it  "should create booking when valida parameters are given" do
       curr_booking = FactoryGirl.build(:booking1)
       expect(curr_booking).to be_valid
+    end
+    
+    it  " should not create booking without 60 minutes gap" do
+      curr_booking = FactoryGirl.create(:booking1)
+      new_booking = FactoryGirl.build(:booking2)
+      expect(new_booking).not_to be_valid
+    end
+    
+    it "should not create booking with duration exceeding 60 mins" do 
+      curr_booking = FactoryGirl.build(:booking3)
+      expect(curr_booking).not_to be_valid
+    end
+    
+    it "should create booking with duration not exceeding 60 mins" do 
+      curr_booking = FactoryGirl.build(:booking4)
+      expect(curr_booking).to be_valid
+    end
+    
+    it "checking the table_exist validation for overlap bookings " do 
+      curr_booking = FactoryGirl.create(:booking5)
+      new_booking = FactoryGirl.build(:booking6)
+      expect(new_booking).not_to be_valid
     end
 end
